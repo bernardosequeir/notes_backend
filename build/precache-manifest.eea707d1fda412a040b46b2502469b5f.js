aa483f57652857162e7f196dbf196ed34da95467
bernardosequeir <bernardosequeir@gmail.com>
2020-10-17 16:46:18 +0100

    aa

---
+++ build/precache-manifest.eea707d1fda412a040b46b2502469b5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd268b3b760fa49952cde4df1e41b273",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9018474df09b538521b3",
+    "url": "/static/css/main.07e03ada.chunk.css"
+  },
+  {
+    "revision": "9aeade65fbc64f2b2f34",
+    "url": "/static/js/2.784046ba.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.784046ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9018474df09b538521b3",
+    "url": "/static/js/main.a38c61ec.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
